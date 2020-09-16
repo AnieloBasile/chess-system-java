f76e1e21ecd0f9b8bcfaf90016df7c783cadd717
Anielo Basile <anielo.jose.basile@gmail.com>
2020-09-16 17:51:35 -0300

    First Class - Position

---
+++ src/Application/Program.java
@@ -0,0 +1,15 @@
+package Application;
+
+import BoardGame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Position posNewPossition = new Position(3, 5);
+		System.out.println(posNewPossition);
+		
+	}
+
+}
