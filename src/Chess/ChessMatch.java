5fc73ff8696066672302b6714fc593be9084efab
Anielo Basile <anielo.jose.basile@gmail.com>
2021-01-11 15:03:28 -0300

    placing pieces on the board

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package Chess;
 
 import BoardGame.Board;
+import BoardGame.Position;
+import Chess.pieces.King;
+import Chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -8,6 +11,7 @@ public class ChessMatch {
 	
 	public ChessMatch() {
 		board = new Board(8,8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -21,4 +25,13 @@ public class ChessMatch {
 		return mat;		
 	}
 
+	private void initialSetup() {
+		//board.placePiece(piece, position);
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+		board.placePiece(new Rook(board, Color.BLACK), new Position(2,2));
+		
+		board.placePiece(new King(board, Color.WHITE), new Position(3,1));
+		board.placePiece(new King(board, Color.BLACK), new Position(3,2));
+
+	}
 }
---
+++ src/Chess/pieces/King.java
@@ -0,0 +1,20 @@
+package Chess.pieces;
+
+import BoardGame.Board;
+import Chess.ChessPiece;
+import Chess.Color;
+
+public class King extends ChessPiece {
+
+	public King(Board board, Color color) {
+		super(board, color);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
+	}
+
+	
+}
